8054010a28117aed7b63e777f07a05491ec86b66
thushan-123 <thushanmadusanka456@gmail.com>
2025-07-23 18:07:01 +0530

    add the arguments

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+import requests
+import argparse
+import queue
+
+arguments = argparse.ArgumentParser(description="Dictories FuZZ tool")
+arguments.add_argument("--word-list", help="Provide a WORD List text file")
+arguments.add_argument("--status-codes", type=list, help="Status codes")
+
+args = arguments.parse_args()
+
+
+
+def fuzz():
+    pass
+
 def main():
     print("Hello from pythonfuzz!")
 
