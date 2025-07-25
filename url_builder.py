dc71383fe904f2af9be2a80a2719b3ce230f8f6a
thushan-123 <thushanmadusanka456@gmail.com>
2025-07-25 23:41:39 +0530

    fix bug  url end / del[-1] fix using rstrip

--- url_builder.py
+++ url_builder.py
@@ -1,5 +1,4 @@
 
-
 class UrlBuilder:
     
     def __init__(self, url: str):
@@ -9,14 +8,8 @@ class UrlBuilder:
         return f"{self.url}/{dir_name}"
     
     def add_more_dir(self, *dir_names):
-        base_url: str = self.url
-        base_path: str = ""
-        
-        for k in dir_names:
-            base_path += f"/{k}"
-        
-        if base_url[-1] == "/":
-            del base_url[-1]
+        base_url: str = self.url.rstrip("/")
+        base_path: str = "/".join(dir_names)
             
-        return base_url + base_path
+        return f"{base_url}/{base_path}"
     
